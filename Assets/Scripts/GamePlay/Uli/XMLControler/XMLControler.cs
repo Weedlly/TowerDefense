f85ffa6b2ea1fba3dd5e31656f6a608c97a141f4
Your Name <khoiledangminh@gmail.com>
2023-04-16 22:10:05 +0700

    Khoi: add tower upgrade feature, replace and remove ui item

--- Assets/Scripts/GamePlay/Uli/XMLControler/XMLControler.cs
+++ Assets/Scripts/GamePlay/Uli/XMLControler/XMLControler.cs
@@ -62,4 +62,33 @@ public class XMLControler : MonoBehaviour
         }
         return false;
     }
+    //khoi update
+     public static bool LocalWriteXML<T>(string filename, T dataList){
+        try{
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+            FileStream stream = new FileStream(Application.persistentDataPath + "/" + filename, FileMode.Create);
+            TextWriter tw = new StreamWriter(stream);
+            xs.Serialize(tw, dataList);
+            stream.Close();
+            return true;
+        }
+        catch(Exception e){
+            Debug.LogError("Exception write  down xml file " + filename +": " + e);
+        }
+        return false;
+    }
+
+    public static T LocalLoadXML<T>(string filename){
+        try{
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using  (var reader = XmlReader.Create(Application.persistentDataPath + "/" + filename)){
+                return (T)serializer.Deserialize(reader);
+            }
+        }
+        catch(Exception e){
+            Debug.LogError("Exception importing xml file " + filename + " :" + e);
+            return default;
+        }
+    }
+    //=====
 }
--- Assets/Scripts/UI/User.cs
+++ Assets/Scripts/UI/User.cs
@@ -4,24 +4,87 @@ using UnityEngine;
 
 public class User : Singleton<User>
 {
-    public string username = "Guest";
-    public int gem = 0;
-    public int star = 0;
-    public float exp = 0.2f;
+    private UserData _data = XMLControler._userData;
+    private int levelCap = 2;
+    
+    void Start(){
+        // Debug.Log("User start");
+        loadUserData();
+        // Debug.Log(getUsername());
+        // _data = XMLControler._userData;
+    }
 
     public void saveUserData(){
-        PlayerPrefs.SetString("username", username);
-	    PlayerPrefs.SetInt("gem", gem);
-	    PlayerPrefs.SetInt("star", star);
-        PlayerPrefs.SetFloat("exp", exp);
-	    PlayerPrefs.Save();
+        XMLControler.LocalWriteXML<UserData>("User.xml", _data);
     }
 
     public void loadUserData(){
-        if (PlayerPrefs.HasKey("username")){
-            username = PlayerPrefs.GetString("username");
-            gem = PlayerPrefs.GetInt("gem");
-            star = PlayerPrefs.GetInt("star");
+        _data = XMLControler.LocalLoadXML<UserData>("User.xml");     
+        // _data = XMLControler.LoadXML<UserData>("XML/User"); 
+    }
+
+    public void generateUserData(){
+        Debug.Log("generating...");
+        _data = XMLControler.LoadXML<UserData>("XML/User");
+        saveUserData();
+    }
+
+    public string getUsername(){
+        return _data.username;
+    }
+
+    public int getStar(){
+        return _data.currentStar;
+    }
+
+    public void StageSuccess(int id, int star, GameObjectEnum.DifficultyTypeEnum difficulty){
+        for(int i = 0; i <=  _data.completetedStageList.Count; i++){
+            if (_data.completetedStageList[i].completetedStageId == id){
+                _data.completetedStageList[i].difficulty = difficulty.ToString();
+                if (_data.completetedStageList[i].star < star){
+                    _data.currentStar += star - _data.completetedStageList[i].star;
+                    _data.completetedStageList[i].star = star;
+                }
+                //saveUserData();
+                return;
+            }
         }
+        CompletetedStageData newStage = new CompletetedStageData();
+        newStage.completetedStageId = id;
+        newStage.difficulty = difficulty.ToString();
+        newStage.star = star;
+        _data.currentStar +=  star;
+        _data.completetedStageList.Add(newStage);
+        //saveUserData();
+
+    }
+
+    public CompletetedStageData getStageData(int id){
+        foreach (var i in _data.completetedStageList){
+            if (i.completetedStageId == id) return i;
+        }
+        return null;
+    }
+
+    public int getTowerLevelById(int id){
+        foreach (var i in _data.towerUpdateList){
+            if (i.towerUpdateId == id) return i.level;
+        }
+        return 0;
+    }
+
+    public bool upgradeTowerById(int id){
+        if (_data.currentStar > 0) {
+            for(int i = 0; i < _data.towerUpdateList.Count; i++){
+                Debug.Log(i);
+                if (_data.towerUpdateList[i].towerUpdateId == id && _data.towerUpdateList[i].level < levelCap) {
+                    _data.towerUpdateList[i].level++;
+                    break;
+                }
+            }
+            saveUserData();
+            return true;
+        }
+        return false;
     }
 }
